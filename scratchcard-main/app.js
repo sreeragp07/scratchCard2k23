3ab5878824bd5fb5f978df240caa4f87136b313b
sreeragp07 <84373517+sreeragp07@users.noreply.github.com>
2023-08-27 00:07:18 +0530

    Updated app.js

--- scratchcard-main/app.js
+++ scratchcard-main/app.js
@@ -2,8 +2,8 @@
 
 $('#card').wScratchPad({
     size        : 20,          // The size of the brush/scratch.
-    bg          : 'images/sam3.jpg',  // Background (image path or hex color).
-    fg          :  'images/sam1.jpg',  // Foreground (image path or hex color).
+    bg          : 'images/better_luck.jpg',  // Background (image path or hex color).
+    fg          :  'images/google-scratch-card.jpg',  // Foreground (image path or hex color).
    
     cursor      : 'pointer' // Set cursor.
   });
