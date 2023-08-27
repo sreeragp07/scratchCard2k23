4da59875f70061396ffca98499c911557fe751eb
sreeragp07 <84373517+sreeragp07@users.noreply.github.com>
2023-08-27 22:06:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-//var num=Math.floor(Math.random()*4)+1;
+// var num = Math.floor(Math.random() * 4) + 1;
 
-$('#card').wScratchPad({
-    size        : 100,          // The size of the brush/scratch.
-    bg          : 'images/better_luck.jpg',  // Background (image path or hex color).
-    fg          :  'images/google-scratch-card.jpg',  // Foreground (image path or hex color).
-   
-    cursor      : 'pointer' // Set cursor.
-  });
+$("#card").wScratchPad({
+  size: 70, // The size of the brush/scratch.
+  // bg: `Images/front.jpg`, // Background (image path or hex color).
+  fg: `Images/google-scratch-card.jpg`, // Foreground (image path or hex color).
+  cursor: "pointer", // Set cursor.
+});
